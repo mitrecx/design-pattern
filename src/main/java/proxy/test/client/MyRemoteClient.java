03cf30916014114f3dec3f8e7c0edef255afa757
Mitrecx <mitrecx@163.com>
2019-06-16 23:06:18 +0800

    RMI客户端

---
+++ src/main/java/proxy/test/client/MyRemoteClient.java
@@ -0,0 +1,31 @@
+package proxy.test.client;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import proxy.test.remote.MyRemote;
+
+/**
+ * @author cx
+ */
+public class MyRemoteClient {
+    public static void main(String[] args) {
+        new MyRemoteClient().go();
+    }
+
+    public void go() {
+        try {
+            MyRemote service = (MyRemote) Naming.lookup("rmi://localhost:8810/RemoteHello");
+            String s = service.sayHello();
+            System.out.println(s);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
